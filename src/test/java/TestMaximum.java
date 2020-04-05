c14eb587ac0e8da11891a69f8ad5519ca4eb8b1f
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-05 13:37:03 +0530

    ADDED:Test minimum three float value

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -20,5 +20,12 @@ public class TestMaximum {
         Integer maxInteger[] = {10,20,30};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[2]);
     }
+
+    @Test
+    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue() {
+        Maximum maximum = new Maximum();
+        Float maxFloat[] = {10.11f, 12.12f, 13.210f};
+        Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[2]);
+    }
 }
 
