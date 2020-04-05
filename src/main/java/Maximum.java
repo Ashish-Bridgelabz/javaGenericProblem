957f523c4ad6a3e418c261fa1dd561fdc20864e5
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-05 13:32:41 +0530

    ADDED:Finded the three maximum floats value

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,4 +1,5 @@
 public class Maximum {
+    //FIND THREE MAXINTEGER VALUE
     public static Integer maximumInteger(Integer[] maxInteger) {
         Integer maximum = 0;
         for (Integer index : maxInteger) {
@@ -7,5 +8,15 @@ public class Maximum {
         }
         return maximum;
     }
+    //FIND THREE MAXFLOATS VALUE
+    public static Float maximumFloat(Float[] maxFloat){
+        Float maximum = 0.0f;
+        for (Float index : maxFloat)  {
+            if (index.compareTo(maximum)>0)
+                maximum = index;
+        }
+        return maximum;
+    }
+
 }
 
