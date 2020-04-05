403dd8dd0f9c71be32da7704a37a4f4639e7bd00
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-06 00:47:08 +0530

    ADDED:Extend the max method and take three parameters

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -5,16 +5,18 @@ public class Maximum<E extends Comparable<E>> {
     E secondValue;
     E thirdValue;
 
+    //DEFAULT CONSTRUCTOR
     public Maximum() {
     }
 
+    //PARAMRTRIZED CONSTRUCTOR
     public Maximum(E firstValue, E secondValue, E thirdValue) {
         this.firstValue = firstValue;
         this.secondValue = secondValue;
         this.thirdValue = thirdValue;
     }
 
-    //Generic method for finding maximum three elements
+    //GENERIC METHOD FOR FINDING MAXIMUM THREE ELEMENTS
     public E findMaximum(E firstValue, E secondValue, E thirdValue) {
         if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
             return firstValue;
@@ -24,9 +26,15 @@ public class Maximum<E extends Comparable<E>> {
             return thirdValue;
     }
 
-    //Test method to internally call
+
+    // INTERNAL CALL FIND MAXIMUM NUMBER
     public <E extends Comparable<E>> E findMaximum() {
+        printMax(findMaximum(firstValue, secondValue, thirdValue));
         return (E) findMaximum(firstValue, secondValue, thirdValue);
+    }
+
+    public void printMax(E findMaximum) {
+        System.out.println("Maximum is:" + findMaximum);
     }
 }
 
