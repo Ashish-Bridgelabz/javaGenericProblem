3dd750e54aa37b897c117192fd93baa032a4b0b1
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-05 23:05:49 +0530

    ADDED:Create generic class to take in three variable of generic type

--- src/main/java/Maximum.java
+++ src/main/java/Maximum.java
@@ -1,13 +1,32 @@
-public class Maximum {
+//public class Maximum {
 
-    public static <E extends Comparable> E maximumValue(E[] maxInteger) {
-        E maximum = maxInteger[0];
-        for (E index : maxInteger) {
-            if (index.compareTo(maximum) > 0)
-                maximum = index;
-        }
-        return maximum;
+public class Maximum<E extends Comparable<E>> {
+    E firstValue;
+    E secondValue;
+    E thirdValue;
+
+    public Maximum() {
     }
 
+    public Maximum(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    //Generic method for finding maximum three elements
+    public E findMaximum(E firstValue, E secondValue, E thirdValue) {
+        if (firstValue.compareTo(secondValue) > 0 && firstValue.compareTo(thirdValue) > 0)
+            return firstValue;
+        else if (secondValue.compareTo(thirdValue) > 0 && secondValue.compareTo(firstValue) > 0)
+            return secondValue;
+        else
+            return thirdValue;
+    }
+
+    //Test method to internally call
+    public <E extends Comparable<E>> E findMaximum() {
+        return (E) findMaximum(firstValue, secondValue, thirdValue);
+    }
 }
 
