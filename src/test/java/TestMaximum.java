79dbb0a9f616b6e54f2b794c0a6d8a1be529c44d
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-05 16:17:30 +0530

    ADDED:Added all the three individual in test case

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -6,34 +6,61 @@ public class TestMaximum {
     @Test
     public void givenThreeIntegerValue_FindMaximum_ShouldReturnMaximumValue_WithGenericMethod() {
         Maximum maximum = new Maximum();
-        Integer maxInteger[] = {30,20,10};
+        Integer maxInteger[] = {30, 20, 10};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[0]);
     }
+
     @Test
     public void givenThreeIntegerValue_FindMaximum_ShouldReturnMaximumValue2_WithGenericMethod() {
         Maximum maximum = new Maximum();
-        Integer maxInteger[] = {20,30,10};
+        Integer maxInteger[] = {20, 30, 10};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[1]);
     }
+
     @Test
     public void givenThreeIntegerValue_FindMaximum_ShouldReturnMaximumValue3_WithGenericMethod() {
         Maximum maximum = new Maximum();
-        Integer maxInteger[] = {10,20,30};
+        Integer maxInteger[] = {10, 20, 30};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[2]);
     }
+
     //MAXIMUM THREE FLOATS VALUE
     @Test
-    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue() {
+    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue_Maximum1() {
+        Maximum maximum = new Maximum();
+        Float maxFloat[] = { 13.210f,10.11f, 12.12f};
+        Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[0]);
+    }
+    @Test
+    public void givenThreeFloatNumberWhenProper_MethodShouldReturnTrue_Maximum2() {
+        Maximum maximum = new Maximum();
+        Float maxFloat[] = {10.11f, 13.210f,12.12f};
+        Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[1]);
+    }
+    @Test
+    public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue_Maximum3() {
         Maximum maximum = new Maximum();
         Float maxFloat[] = {10.11f, 12.12f, 13.210f};
         Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[2]);
     }
+
     //MAXIMUM THREE STRING CHARACTER
     @Test
-    public void givenThreeString_WhenProper_MethodShouldReturnMaximum() {
+    public void givenThreeString_WhenProper_MethodShouldReturnMaximum1() {
         Maximum maximum = new Maximum();
-        String maxString[] = {"bridgelabz", "accenture", "capgemini"};
+        String maxString[] = {"gyan","Dev", "ashish"};
+        Assert.assertEquals(maximum.maximumString(maxString), maxString[0]);
+    }
+    @Test
+    public void givenThreeString_WhenProper_MethodShouldReturnMaximum2() {
+        Maximum maximum = new Maximum();
+        String maxString[] = {"Dev","gyan", "ashish"};
+        Assert.assertEquals(maximum.maximumString(maxString), maxString[1]);
+    }
+    @Test
+    public void givenThreeString_WhenProper_MethodShouldReturnMaximum3() {
+        Maximum maximum = new Maximum();
+        String maxString[] = {"Dev", "ashish","gyan"};
         Assert.assertEquals(maximum.maximumString(maxString), maxString[2]);
     }
 }
-
