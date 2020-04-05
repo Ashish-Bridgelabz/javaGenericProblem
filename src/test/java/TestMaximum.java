c0baf0389a1fa7a9b07ab390aab86d243af7c690
Ashish-Bridgelabz <asaschoudhary49@gmail.com>
2020-04-05 14:35:11 +0530

    ADDED:Test case to finded maximum three string

--- src/test/java/TestMaximum.java
+++ src/test/java/TestMaximum.java
@@ -2,6 +2,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestMaximum {
+    //MAXIMUM THREE INTEGERS VALUE
     @Test
     public void givenThreeIntegerValue_FindMaximum_ShouldReturnMaximumValue_WithGenericMethod() {
         Maximum maximum = new Maximum();
@@ -20,12 +21,19 @@ public class TestMaximum {
         Integer maxInteger[] = {10,20,30};
         Assert.assertEquals(maximum.maximumInteger(maxInteger), maxInteger[2]);
     }
-
+    //MAXIMUM THREE FLOATS VALUE
     @Test
     public void givenThreeFloatNumber_WhenProper_MethodShouldReturnTrue() {
         Maximum maximum = new Maximum();
         Float maxFloat[] = {10.11f, 12.12f, 13.210f};
         Assert.assertEquals(maximum.maximumFloat(maxFloat), maxFloat[2]);
+    }
+    //MAXIMUM THREE STRING CHARACTER
+    @Test
+    public void givenThreeString_WhenProper_MethodShouldReturnMaximum() {
+        Maximum maximum = new Maximum();
+        String maxString[] = {"bridgelabz", "accenture", "capgemini"};
+        Assert.assertEquals(maximum.maximumString(maxString), maxString[2]);
     }
 }
 
